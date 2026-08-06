ead156eac49eee831dae8b5f2221849e71c90036
tabinath194-afk <tabinath194@gmail.com>
2026-08-06 21:17:13 +0530

    Add: binary-tree-diameter - submission-0

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-0.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = maxHeight(root.left);
+        int rightHeight = maxHeight(root.right);
+        int diameter = leftHeight + rightHeight;
+        int sub = Math.max(diameterOfBinaryTree(root.left),
+                           diameterOfBinaryTree(root.right));
+        return Math.max(diameter, sub);
+    }
+
+    public int maxHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxHeight(root.left), maxHeight(root.right));
+    }
+}
